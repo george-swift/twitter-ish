b96be3f8e34dbf7040252013ce3dd474b0574f88
George <71434803+george-swift@users.noreply.github.com>
2021-04-16 12:01:43 +0100

    Add layout and route for new user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'app_pages#home'
   get '/about', to: 'app_pages#about'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
