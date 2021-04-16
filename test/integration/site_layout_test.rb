1a817244a375d2986922d2eb9a3938b553ad114b
George <71434803+george-swift@users.noreply.github.com>
2021-04-16 03:18:30 +0100

    Add integration test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test 'layout links' do
+    get root_path
+    assert_template 'app_pages/home'
+    assert_select 'a[href=?]', about_path
+  end
+end
