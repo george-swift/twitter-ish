5ddd3c2f1fbb33023d2ad8996508008bc2394190
George <71434803+george-swift@users.noreply.github.com>
2021-04-17 08:10:49 +0100

    Add flash message to user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle successful signup
+      flash[:success] = 'Welcome to Twitter-ish'
+      redirect_to @user
     else
       render 'new'
     end
