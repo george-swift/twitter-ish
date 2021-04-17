f06337930585ae8b8f79da3ef44e2d8405b401ac
George <71434803+george-swift@users.noreply.github.com>
2021-04-17 10:09:30 +0100

    Add named routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'app_pages#home'
   get '/about', to: 'app_pages#about'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
