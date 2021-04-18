2f815522eceeccbf55ec502b76c7d997a7cba653
George <71434803+george-swift@users.noreply.github.com>
2021-04-18 05:49:29 +0100

    Test implementation of logging out users

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:jarvis)
+  end
+
   test 'log in with invalid information' do
     get login_path
     assert_template 'sessions/new'
@@ -9,5 +13,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test 'log in with valid information and subsequent log out' do
+    get login_path
+    post login_path params: { session: { username: @user.username, fullname: @user.fullname } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    delete logout_path
+    assert_not user_logged_in?
+    assert_redirected_to root_url
   end
 end
