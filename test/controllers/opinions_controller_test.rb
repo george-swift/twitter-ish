5b0745305805047eb204135fc506013d592402a6
George <71434803+george-swift@users.noreply.github.com>
2021-04-21 14:03:16 +0100

    Add integration tests for deleting opinions

--- test/controllers/opinions_controller_test.rb
+++ test/controllers/opinions_controller_test.rb
@@ -18,4 +18,13 @@ class OpinionsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
+
+  test 'should redirect destroy for wrong opinion' do
+    log_in_as(users(:jarvis))
+    opinion = opinions(:devs)
+    assert_no_difference 'Opinion.count' do
+      delete opinion_path(opinion)
+    end
+    assert_redirected_to users_url
+  end
 end
