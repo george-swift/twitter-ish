765c1ba862202f7caac0a1720da814a10f707c21
George <71434803+george-swift@users.noreply.github.com>
2021-04-21 14:38:28 +0100

    Add Following model and implement multiple index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_174824) do
+ActiveRecord::Schema.define(version: 2021_04_21_131250) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2021_04_20_174824) do
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_followings_on_followed_id"
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
+    t.index ["follower_id"], name: "index_followings_on_follower_id_and_follower_id", unique: true
   end
 
   create_table "opinions", force: :cascade do |t|
