a91d569a4baf99525128c1d194671526682fefd6
George <71434803+george-swift@users.noreply.github.com>
2021-04-21 19:23:00 +0100

    Add routes for user followings

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
     end
   end
   resources :opinions, only: %i[create destroy]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :followings, only: %i[create destroy]
 end
