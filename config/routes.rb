f7a35792b4475fda0d5e7483774f0c09220e0e86
George <71434803+george-swift@users.noreply.github.com>
2021-04-21 16:45:51 +0100

    Add member routes and embed stats in views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :opinions, only: %i[create destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
